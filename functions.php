d28956e11800504cb2d826b3d7f919c6b60a226c
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2025-06-12 13:44:55 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,12 +24,28 @@ error_reporting( E_ALL & ~E_NOTICE & ~E_WARNING );
 /**
  * Remove the sidebar
  *
- * @see  https://njengah.com/woocommerce-remove-sidebar-from-product-page/
+ * @see https://njengah.com/woocommerce-remove-sidebar-from-product-page/
  */
 
 add_action( 'get_header', function() {
     remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
 } );
+
+
+
+/**
+ * Add a "sold out"-badge
+ *
+ * @see https://www.cloudways.com/blog/add-woocommerce-sold-out-badge/
+ */
+/*
+add_action( 'woocommerce_before_shop_loop_item_title', function() {
+    global $product;
+
+    if ( !$product->is_in_stock() ) {
+        echo '<span class="now_sold">Now Sold</span>';
+    }
+}); */
 
 
 
